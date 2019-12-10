5aac0b10232ab719de885f7d7f39311c7a77c75e
ytyeoh <ytyeoh@me.com>
2019-12-10 10:47:17 +0800

    first init

---
+++ client/word.js
@@ -0,0 +1,90 @@
+// header
+var brand = "Deansbrook Pre-School"
+var menu = {
+  menu1:'Welcome', menu2: 'Enrichment', menu3: 'Location', menu4:'Programme', menu5: 'Gallery', menu6: 'Contact'
+}
+
+// background images
+var background1 = "hero_4.jpg"
+var background2 = "hero_4.jpg"
+// section1
+var caption1a = '<span class="fa fa-star text-primary"></span> Welcome To Deansbrook PreSchool <span class="fa fa-star text-primary"></span>'
+var captionH1 = 'Our mission is to plant the seeds of lifelong learning'
+var welcomeImageR1 = 'food-1.jpg'
+var welcomeImageS1 = 'img_1z.jpg'
+var captionH2a = 'Welcome!'
+var caption1b = 'A warm welcome to all our kindergarteners! We are ready for the coming years learning adventure. Kindergarten is a time for children to expand their love of learning, general knowledge, their ability to get along with others, and their interest in reaching out to the world. Our education program is set to meet all that and provide a nurturing environment. While kindergarten marks an important transition, it is important that children still get to be children. "Your children are in for a treat!" Kindergarten life is sweet with all the fun and exciting activities. Join Deansbrook, where dreams are born and inspired.'
+
+// section2 room
+var captionH2b = 'Enrichment'
+var caption2a= "Make learning fun for the children but at the same time educational. "
+var rooms = {
+  room1:{
+    image: "img_1a.jpg", title: 'Sing&Play', price: 'FREE / monthly', desc: 'It`s all about fun really. A fun, exciting at energetic approach to learn music, songs, dance and play! Kids from ages 2-6 yrs.old are welcome! '
+  },
+  room2: {
+    image: "img_2a.jpg", title: 'Brain Development Programme (DeBrain ~ Pengenius)', price: 'RM100 / per monthly', desc: 'DeBrain is the leading wholebrain programme in Malaysia which develop children`s photographic memory building creativity & strengthen children`s concentration for learning success @ personality development.'
+  },
+  room3:{
+    image: "img_3a.jpg", title: 'Speech and Drama (Treasure Chest Education)', price: 'RM95 / per monthly', desc: 'Our Speech, Drama & Brain Development Programme focuses on several key aspects we believe to be essential for a young learner¡¯s development. We adopt STELLAR (Strategies for English Language Learning and Reading) as well as inquiry-based learning teaching methods to help our young learners acquire confidence in effective verbal communication, public speaking, presentation, critical thinking and collaborative skills. Unlike contemporary teacher-led speech & drama classes, our curriculum prioritizes extensive interaction between our young learners, as we challenge them to think, analyze and solve problems through the exciting medium of drama tools such as poetry, theatre, mime, puppetry, role-play, masks and story-telling.' 
+  }
+}
+var callText = "Our mission is to plant the seeds of lifelong learning."
+var call = 'contact now'
+
+// section3
+var caption3a = '3C, Lorong Delima Satu, 11060 Georgetown Penang.'
+var galeryImages = {
+  1: 'slider-1.jpg', 2: 'slider-2.jpg', 3: 'slider-3.jpg', 4: 'slider-4.jpg', 5: 'slider-5.jpg'
+}
+
+// section4
+var captionH2d = "Programme"
+var caption4a = 'Our programmes are divided into three groups: Nursery, Kindergarten, Primary School Care'
+var attrations = {
+  1:{
+    title: "Nursery", image: 'img_1.jpg', desc: 'The environment set for the children spells MONTESSORI. Through working with didactic Montessori learning materials, children are stimulated to acquire new concepts and knowledge.'
+  },
+  2:{
+    title: "Kindergarten", image: 'img_2.jpg', desc: 'The programme prepares the children for a smooth transition from kindergarten to primary school. Children begin their formal mastery of the 3Rs ¨C Reading, Writing & Arithmetic. The programme, though teacher-directed, remains child-centered.'
+  },
+  3:{
+    title: "Primary School Care", image: 'img_3.jpg', desc: 'Before and After School Care caters to primary school children of ages 7 to 12. We will providing the children with proper baths and meals, at the same time guiding the children in their homework and providing them with extra tuition when necessary'
+  }
+}
+
+// section5
+var tours = {
+  1:{
+    title: "Chinese New Year Celebration", image: 'img_1g.jpg', desc: 'Chinese festival that celebrates the beginning of a new year on the traditional Chinese calendar.', url: 'http://www.kikia.co'
+  },
+  2:{
+    title: "Art & Craft", image: 'img_2g.jpg', desc: 'To provides immediate and lasting cognitive benefits but also creates a unique bonding experience', url: 'http://www.kikia.co'
+  },
+  3:{
+    title: "Cooking Time", image: 'img_3g.jpg', desc: 'Experience of creating meals to  build children self-confidence and lay the foundation for healthy eating habits.', url: 'http://www.kikia.co'
+  },
+  4:{
+    title: "Water Play", image: 'img_4g.jpg', desc: 'Water play is a fun way of learning for young children; as it expand their imagination, physical development, encourages problem solving, numeracy and language.', url: 'http://www.kikia.co'
+  },
+  5:{
+    title: "Children Day", image: 'img_5g.jpg', desc: 'For every child, every right. ', url: 'http://www.kikia.co'
+  },
+  6:{
+    title: "Concert", image: 'img_6g.jpg', desc: 'End of Year Graduation and Annual Ceremony', url: 'http://www.kikia.co'
+  }
+}
+
+// section6
+var custId = 'ytyeoh@me.com'
+var address1 = '3C, Lorong Delima Satu '
+var address2 = "Georgetown, 11060 Penang."
+var mobile = "016-465 8595"
+
+// footer
+var facebookUrl = 'https://www.facebook.com/kikiatour'
+
+
+
+
+
